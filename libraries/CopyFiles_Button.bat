2a744edb42791989d838d2f7d48e23fc5dcef8a9
JaidenK <king.jaiden@yahoo.com>
2025-01-08 12:40:38 -0800

    Add some batch files to speed up building the code

---
+++ libraries/CopyFiles_Button.bat
@@ -0,0 +1,5 @@
+xcopy /s /y ES_Framework "C:\Users\kingj\OneDrive\Documents\Arduino\libraries\ES_Framework"
+xcopy /s /y KTaNE "C:\Users\kingj\OneDrive\Documents\Arduino\libraries\KTaNE"
+xcopy /s /y KTaNE_Button "C:\Users\kingj\OneDrive\Documents\Arduino\libraries\KTaNE_Button"
+
+@pause
---
+++ libraries/CopyFiles_Timer.bat
@@ -0,0 +1,5 @@
+xcopy /s /y ES_Framework "C:\Users\kingj\OneDrive\Documents\Arduino\libraries\ES_Framework"
+xcopy /s /y KTaNE "C:\Users\kingj\OneDrive\Documents\Arduino\libraries\KTaNE"
+xcopy /s /y KTaNE_Timer "C:\Users\kingj\OneDrive\Documents\Arduino\libraries\KTaNE_Timer"
+
+@pause
